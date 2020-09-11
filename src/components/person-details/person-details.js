bbd616ab9c2491cb4e155e27522dcd589ac85627
K2-m5 <56111100+K2-m5@users.noreply.github.com>
2020-09-12 01:28:02 +0300

    feat: add spinner and error function

--- src/components/person-details/person-details.js
+++ src/components/person-details/person-details.js
@@ -1,6 +1,8 @@
 import React, { Component, Fragment } from 'react';
 import SwapiService from '../../services';
 import ErrorButton from '../error-button';
+import ErrorIndicator from '../erorr-indicator';
+import Spinner from '../spinner';
 
 import './person-details.css';
 
@@ -9,8 +11,9 @@ export default class PersonDetails extends Component {
   swapiService = new SwapiService();
 
   state = {
-    person: null,
+    person: {},
     loading: true,
+    error: false
   }
 
   updatePerson() {
@@ -21,9 +24,23 @@ export default class PersonDetails extends Component {
     }
 
     this.swapiService.getPerson(personId)
-      .then((person) => {
-        this.setState({ person });
-      });
+      .then(this.onPersonLoaded)
+      .catch(this.onError);
+  };
+
+  onPersonLoaded = (person) => {
+    this.setState({
+      person,
+      loading: false,
+      error: false
+    });
+  };
+
+  onError = (err) => {
+    this.setState({
+      loading: false,
+      error: true
+    })
   }
 
   componentDidMount() {
@@ -41,34 +58,52 @@ export default class PersonDetails extends Component {
     if (!this.state.person) {
       return <span>Select person</span>
     }
-    
-    const { id, name, gender, birthYear, eyeColor } = this.state.person;
+
+    const { loading, person, error } = this.state;
+
+    const hasData = !(error || loading);
+    const errorMessage = error ? <ErrorIndicator /> : null;
+    const spinner = loading ? <Spinner /> : null;
+    const content = hasData ? <PersonDetailsView person={person} /> : null;
 
     return (
       <div className="person-details card">
-        <img className="person-image"
-          src={`https://starwars-visualguide.com/assets/img/characters/${id}.jpg`} 
-          alt='hero'/>
-
-        <div className="card-body">
-          <h4>{name}</h4>
-          <ul className="list-group list-group-flush">
-            <li className="list-group-item">
-              <span className="term">Gender</span>
-              <span>{gender}</span>
-            </li>
-            <li className="list-group-item">
-              <span className="term">birthday year</span>
-              <span>{birthYear}</span>
-            </li>
-            <li className="list-group-item">
-              <span className="term">Eye color</span>
-              <span>{eyeColor}</span>
-            </li>
-          </ul>
-          <ErrorButton />
-        </div>
+        {errorMessage}
+        {spinner}
+        {content}
       </div>
     )
   }
 }
+
+const PersonDetailsView = ({ person }) => {
+
+  const { id, name, gender, birthYear, eyeColor } = person;
+
+  return (
+    <Fragment>
+      <img className="person-image"
+        src={`https://starwars-visualguide.com/assets/img/characters/${id}.jpg`} 
+        alt='hero'/>
+
+      <div className="card-body">
+        <h4>{name}</h4>
+        <ul className="list-group list-group-flush">
+          <li className="list-group-item">
+            <span className="term">Gender</span>
+            <span>{gender}</span>
+          </li>
+          <li className="list-group-item">
+            <span className="term">birthday year</span>
+            <span>{birthYear}</span>
+          </li>
+          <li className="list-group-item">
+            <span className="term">Eye color</span>
+            <span>{eyeColor}</span>
+          </li>
+        </ul>
+        <ErrorButton />
+      </div>
+    </Fragment>
+  );
+};
