03f711aec674df71c0574acef357e571015ae558
K2-m5 <56111100+K2-m5@users.noreply.github.com>
2020-09-24 01:11:59 +0300

    feat: add hight componets context

---
+++ src/components/hoc-helpers/with-swapi-service.js
@@ -0,0 +1,22 @@
+import React from 'react';
+import { SwapiServiceConsumer } from '../swapi-service-context';
+
+
+const withSwapiService = (Wrapped, mapServiceToProps) => {
+
+  return (props) => {
+    return (<SwapiServiceConsumer>
+      {
+        (swapiService) => {
+          const serviceProps = mapServiceToProps(swapiService);
+
+          return(
+            <Wrapped {...props} {...serviceProps}/>
+          )
+        }
+      }
+    </SwapiServiceConsumer>)
+  }
+}
+
+export default withSwapiService;
